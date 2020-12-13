68391f200ee3e4acbcd7a540d5b0a4c8eb73db94
Hrishikesh Paul <hrpaul@iu.edu>
2020-12-13 12:13:57 -0500

    Auto-generated commit

--- assets/static.ts
+++ assets/static.ts
@@ -1,4 +1,13 @@
-function URL(date: string, page: number) {
+/**
+ * A function to build the URL based on the last 30 days date
+ * and the page number
+ * 
+ * @param date Date 30 days ago
+ * @param page Page number
+ * 
+ * @returns URL string
+ */
+function URL(date: string, page: number): string {
   return `https://api.github.com/search/repositories?q=created:${date}&sort=stars&order=desc&page=${page}`;
 }
 
